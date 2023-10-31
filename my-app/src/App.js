94a27fadfe26ffb59dd6e79a084a6831ff537e15
Karlox01 <orlandinikarlo@gmail.com>
2023-10-31 21:02:49 +0000

    initial commit -- forms

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,10 @@
 import css from './App.module.css';
+import ControlledForm from './components/ControlledForm';
 
 function App() {
   return (
     <div className={css.App}>
-      {/* Add your components here */}
+      <ControlledForm />
     </div>
   );
 }
