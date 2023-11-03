23e785b07f1db60e3ee1a448bc448d9fb660ecab
Karlox01 <orlandinikarlo@gmail.com>
2023-11-03 20:40:19 +0000

    form search end lesson

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,16 @@ import css from './App.module.css';
 import ControlledForm from './components/ControlledForm';
 import UncontrolledForm from './components/UncontrolledForm';
 import { SearchBar3 } from './components/SearchBar3';
+import { SearchBar1 } from './components/SearchBar1';
+import { SearchBar2 } from './components/SearchBar2';
 
 function App() {
   return (
     <div className={css.App}>
       {/* <ControlledForm /> */}
       {/* <UncontrolledForm /> */}
+      {/* <SearchBar1 />
+      <SearchBar2 /> */}
       <SearchBar3 />
     </div>
   );
